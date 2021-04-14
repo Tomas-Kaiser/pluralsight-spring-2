0e568a98e52dafa7c6b28df43ad618ea82eb878f
Tomas Kaiser <tomas.kaiser06@gmail.com>
2021-04-14 21:22:59 +0200

    Add beans

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,22 @@
+import com.tomas.repository.HibernateSpeakerRepositoryImpl;
+import com.tomas.repository.SpeakerRepository;
+import com.tomas.service.SpeakerService;
+import com.tomas.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService() {
+        SpeakerServiceImpl service = new SpeakerServiceImpl(getSpeakerRepository());
+//        service.setRepository(getSpeakerRepository());
+        return service;
+    }
+
+    @Bean(name = "speakerRepository")
+    public SpeakerRepository getSpeakerRepository(){
+        return new HibernateSpeakerRepositoryImpl();
+    }
+}
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,18 @@
 import com.tomas.service.SpeakerService;
 import com.tomas.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        System.out.println(service.findAll().get(0).getFirstName());
+//        SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
+
+        System.out.println(service.findAll().get(1).getFirstName());
     }
 }
